c109bb4768c35c4846495bda5567cf514c171fa8
shangshicc <shangshicc@gmail.com>
2016-04-20 17:39:50 +0800

    update loading UI

--- app/src/main/java/com/shangshicc/android/timerclock/MainActivity.java
+++ app/src/main/java/com/shangshicc/android/timerclock/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.Timer;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/com/shangshicc/android/timerclock/TimerClockView.java
+++ app/src/main/java/com/shangshicc/android/timerclock/TimerClockView.java
@@ -6,11 +6,13 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import java.util.Timer;
@@ -29,15 +31,19 @@ public class TimerClockView extends View implements View.OnClickListener{
     private int mTextSize;
     private int mTextColor;
     private String mText;
-
+    private RectF mRectF;
+    private float mUpdateAngle;
+    private volatile float mAngle;
     private ClockHandler mHandler;
     private Timer mTimer;
 
     private int mTimerClockState;
     public static final int TIMER_CLOCK_STOP = 0;
     public static final int TIMER_CLOCK_START = 1;
+    public static final int TIMER_CLOCK_PAUSE = 2;
 
     public static final int UPDATE_CLOCK_WHAT = 0;
+
     public TimerClockView(Context context) {
         super(context);
         mContext = context;
@@ -65,6 +71,10 @@ public class TimerClockView extends View implements View.OnClickListener{
         parseAttrs(ta);
         configPaint();
         setOnClickListener(this);
+
+        mAngle = 0;
+        mUpdateAngle = 360 / mSeconds;
+        mRectF = new RectF(-(mCircleRad + 10),-(mCircleRad + 10),mCircleRad + 10,mCircleRad + 10);
     }
 
     public void configData(){
@@ -74,6 +84,10 @@ public class TimerClockView extends View implements View.OnClickListener{
         mTextSize = 60;
         mText = mContext.getString(R.string.start_text);
         mTextColor = Color.BLACK;
+
+        mUpdateAngle = 360 / mSeconds;
+        mAngle = 0;
+        mRectF = new RectF(-(mCircleRad + 10),-(mCircleRad + 10),mCircleRad + 10,mCircleRad + 10);
     }
 
     public void parseAttrs(TypedArray ta){
@@ -83,7 +97,7 @@ public class TimerClockView extends View implements View.OnClickListener{
         mCircleRad = ta.getFloat(R.styleable.TimerClockView_circle_radius, 200);
         mTextSize = ta.getInt(R.styleable.TimerClockView_text_size, 60);
         mText = ta.getString(R.styleable.TimerClockView_start_text);
-        mTextColor = ta.getColor(R.styleable.TimerClockView_text_color,Color.BLACK);
+        mTextColor = ta.getColor(R.styleable.TimerClockView_text_color, Color.BLACK);
         if(mText == null || mText.equals("")){
             mText = mContext.getString(R.string.start_text);
         }
@@ -99,9 +113,8 @@ public class TimerClockView extends View implements View.OnClickListener{
     public void configClockPaint(){
         mCirclePaint.setAntiAlias(true);
         mCirclePaint.setStyle(Paint.Style.STROKE);
-        mCirclePaint.setStrokeWidth(5);
-        mCirclePaint.setTextSize(mTextSize);
         mCirclePaint.setColor(mClockColor);
+        mCirclePaint.setStrokeWidth(10);
     }
 
     public void configTextPaint(){
@@ -116,27 +129,29 @@ public class TimerClockView extends View implements View.OnClickListener{
         super.onDraw(canvas);
         canvas.translate(getMeasuredWidth() / 2, getMeasuredHeight() / 2);
         canvas.drawCircle(0, 0, mCircleRad, mCirclePaint);
-        drawText(mTextPaint, canvas, mText);
+        canvas.drawCircle(0, 0, mCircleRad + 20, mCirclePaint);
+
+        if(mTimerClockState == TIMER_CLOCK_START) {
+            canvas.drawArc(mRectF, 0, mAngle, false, mCirclePaint);
+        }
+
+        Rect bounds = new Rect();
+        mTextPaint.getTextBounds(mText, 0, mText.length(), bounds);
+        canvas.drawText(mText, -bounds.width() / 2, bounds.height() / 2, mTextPaint);
     }
 
-    private void drawText(Paint paint, Canvas canvas, String text){
-        Rect bounds = new Rect();
-        paint.getTextBounds(text, 0, text.length(), bounds);
-        canvas.drawText(text, -bounds.width() / 2, bounds.height() / 2, paint);
-    }
 
     @Override
     public void onClick(View v) {
         if(mTimerClockState == TIMER_CLOCK_STOP) {
             mTimer = new Timer(true);
-            mTimer.schedule(new ClockTimerTask(),1000,1000);
-            invalidate();
+            mTimer.schedule(new ClockTimerTask(), 1000, 1000);
             mTimerClockState = TIMER_CLOCK_START;
         }else{
             mTimer.cancel();
             mTimer = null;
             mTimerClockState = TIMER_CLOCK_STOP;
-        }
+          }
     }
 
     private final class ClockTimerTask extends TimerTask{
@@ -166,6 +181,7 @@ public class TimerClockView extends View implements View.OnClickListener{
                 }
                 mText = String.valueOf(mSeconds);
                 mSeconds--;
+                mAngle += mUpdateAngle;
                 invalidate();
             }
         }
